bfc7c40b3c946323d2029a581cc9dd394a771e2d
Joost Froon <joostfroon@Joosts-Air.home>
2023-02-01 16:18:20 +0100

    data is not availible on 404 page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,26 +21,28 @@ const raleway = Raleway({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
-  const { data: { personal } } = pageProps as Props;
+  const { data } = pageProps as Props;
 
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle fontFamily={raleway.style.fontFamily} />
       <Normalize />
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <title>{personal.description} | {personal.name}</title>
-        <meta name="title" content={personal.title} />
-        <meta name="description" content={personal.description} />
-        <meta name="theme-color" content="#000" />
-        <meta property="og:title" content={personal.title} />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content={personal.image.url} />
-        <meta property="og:description" content={personal.description} />
-        <meta property="og:url" content={personal.website} />
-        <meta property="og:image:alt" content={personal.title} />
-        <meta name="application-name" content={personal.name} />
-      </Head>
+      {data?.personal && (
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <title>{data.personal.description} | {data.personal.name}</title>
+          <meta name="title" content={data.personal.title} />
+          <meta name="description" content={data.personal.description} />
+          <meta name="theme-color" content="#000" />
+          <meta property="og:title" content={data.personal.title} />
+          <meta property="og:type" content="website" />
+          <meta property="og:image" content={data.personal.image.url} />
+          <meta property="og:description" content={data.personal.description} />
+          <meta property="og:url" content={data.personal.website} />
+          <meta property="og:image:alt" content={data.personal.title} />
+          <meta name="application-name" content={data.personal.name} />
+        </Head>
+      )}
       <Component {...pageProps} />
     </ThemeProvider>
   );
